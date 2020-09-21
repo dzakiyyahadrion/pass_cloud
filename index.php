061c4660cf5d7dcac3d6d965843993dd385cce34
dzakiyyahadrion <55857729+dzakiyyahadrion@users.noreply.github.com>
2020-09-21 22:03:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <h1>My First Heading</h1>
-<img src="SS Qwiklabs.jpg">
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fid.foursquare.com%2Fv%2Ffakultas-sains-dan-teknologi-uin-syarif-hidayatullah-jakarta%2F4d0991035edd5481bf5f019b&psig=AOvVaw2jlhpnANfg4lguULJDLqrN&ust=1600786919224000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMie9JnC-usCFQAAAAAdAAAAABAD">
 <p>My first paragraph.</p>
 
 </body>
