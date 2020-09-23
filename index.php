1d89a8f53e10975a7f2fdfe15baecddfc7ece6c8
dzakiyyahadrion <55857729+dzakiyyahadrion@users.noreply.github.com>
2020-09-23 11:17:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 <html>
 <body>
 
-<h1>My First Heading</h1>
-<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fid.foursquare.com%2Fv%2Ffakultas-sains-dan-teknologi-uin-syarif-hidayatullah-jakarta%2F4d0991035edd5481bf5f019b&psig=AOvVaw2jlhpnANfg4lguULJDLqrN&ust=1600786919224000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMie9JnC-usCFQAAAAAdAAAAABAD">
-<p>My first paragraph.</p>
+<h1>Tugas PASS Cloud</h1>
+<p>Annisa Dzakiyyahtul Hanifah (11180910000048)</p>
 
 </body>
 </html>
